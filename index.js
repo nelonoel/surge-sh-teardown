79f38d0ed91d29d52720088ebcd2b6b1b65d04b0
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2019-11-25 17:16:30 +0800

    Teardown on deleted branches only

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const core = require("@actions/core");
 const surge = require("surge");
 
 try {
-	const { ref } = require(process.env.GITHUB_EVENT_PATH);
-	const template = core.getInput('template');
-	const domain = template.replace(/\$BRANCH/ig, ref);
+	const { ref, ref_type } = require(process.env.GITHUB_EVENT_PATH);
 
-	process.env.SURGE_LOGIN = core.getInput("login");
-	process.env.SURGE_TOKEN = core.getInput("token");
+	if (ref_type === 'branch') {
+		const template = core.getInput('template');
+		const domain = template.replace(/\$BRANCH/ig, ref);
 
-	surge({ default: "teardown" })([domain]);
+		process.env.SURGE_LOGIN = core.getInput("login");
+		process.env.SURGE_TOKEN = core.getInput("token");
+
+		surge({ default: "teardown" })([domain]);
+	}
 } catch (error) {
 	core.warning(error);
 }
